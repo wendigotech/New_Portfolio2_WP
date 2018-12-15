af282bf3cd482458b298c1e1991d30e54d469d2e
MB <wendigotech@gmail.com>
2018-12-16 00:38:16 +0100

    6

--- header.php
+++ header.php
@@ -27,10 +27,10 @@
                 <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="100">
                     <div class="container">
                         <div class="navbar-translate">
-                            <?php if ( ! has_custom_logo() ) : ?><div>
-                                <a rel="home" class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a>
-                            </div>
-                            <?php else : ?><?php the_custom_logo(); ?><?php endif; ?>
+                            
+                            <a href="#" rel="home" class="lrg-logo">
+    <img src="file:///E:/Wendigo_new/OneDrive/Web_Design/Personal_Projects/New_Portfolio2/assets/gfx/white_logo_transparent_background.png" width="200"/>
+</a>
                             <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-bar bar1"></span>
                                 <span class="navbar-toggler-bar bar2"></span>
