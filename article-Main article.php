d3b71b84098baa96a7b5bc7d95d6dd4e46c3e393
MB <wendigotech@gmail.com>
2018-12-16 00:04:06 +0100

    3

--- article-Main article.php
+++ article-Main article.php
@@ -1,8 +1,7 @@
 <article <?php post_class( 'mb-5' ); ?> id="post-<?php the_ID(); ?>">
                                                 <header class="entry-header">
                                                     <?php if ( is_singular() ) : ?><h2><?php the_title(); ?></h2><?php else : ?><h2><a href="<?php echo esc_url( get_permalink() ); ?>"><?php the_title(); ?></a></h2><?php endif; ?>
-                                                    <div class="entry-meta">
-</div>
+                                                    
                                                 </header>
                                                 <?php
         if ( has_post_thumbnail() ) {
@@ -14,6 +13,6 @@
                                                 <div class="entry-content">
                                                     <?php the_excerpt( ); ?>
                                                 </div>
-                                                <a class="btn btn-primary ml-1" href="<?php echo esc_url( get_permalink() ); ?>"><?php _e( 'Read More', 'wdg1' ); ?></a>
+                                                <a class="btn btn-primary float-right" href="<?php echo esc_url( get_permalink() ); ?>"><?php _e( 'Read More', 'wdg1' ); ?></a>
                                                 
                                             </article>
