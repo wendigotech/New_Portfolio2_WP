e213a1adb3a9ad972e589ce0baf4199a80084e9d
MB <wendigotech@gmail.com>
2018-12-22 01:44:07 +0100

    15

--- functions.php
+++ functions.php
@@ -257,13 +257,17 @@ if ( ! function_exists( 'st2_enqueue_scripts' ) ) :
     wp_deregister_script( 'bootstrapswitch' );
     wp_enqueue_script( 'bootstrapswitch', get_template_directory_uri() . '/components/blk-design-system-html-v1.0.0/assets/js/plugins/bootstrap-switch.js', false, null, true);
 
-    wp_enqueue_script( 'imagesloadedpkgd', get_template_directory_uri() . '/assets/js/imagesloaded.pkgd.min.js' );
+    wp_deregister_script( 'imagesloadedpkgd' );
+    wp_enqueue_script( 'imagesloadedpkgd', get_template_directory_uri() . '/assets/js/imagesloaded.pkgd.min.js', false, null, true);
 
-    wp_enqueue_script( 'charming', get_template_directory_uri() . '/assets/js/charming.min.js' );
+    wp_deregister_script( 'charming' );
+    wp_enqueue_script( 'charming', get_template_directory_uri() . '/assets/js/charming.min.js', false, null, true);
 
-    wp_enqueue_script( 'tweenmax', get_template_directory_uri() . '/assets/js/TweenMax.min.js' );
+    wp_deregister_script( 'tweenmax' );
+    wp_enqueue_script( 'tweenmax', get_template_directory_uri() . '/assets/js/TweenMax.min.js', false, null, true);
 
-    wp_enqueue_script( 'diagonal', get_template_directory_uri() . '/assets/js/diagonal.js' );
+    wp_deregister_script( 'diagonal' );
+    wp_enqueue_script( 'diagonal', get_template_directory_uri() . '/assets/js/diagonal.js', false, null, true);
 
     /* Pinegrow generated Enqueue Scripts End */
 
